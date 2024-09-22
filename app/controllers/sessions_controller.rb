3e1871b7366e105b33cac198ce21bf3220999844
jais marine <jaismarine@MacBook-Pro-jais.local>
2024-09-22 23:20:50 +0700

    feat: add session controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+    def create
+      user = User.find_by(email: params[:email])
+      
+      # Authenticate the user using bcrypt's `authenticate` method
+      if user && user.authenticate(params[:password])
+        # Save the user ID in the session
+        session[:user_id] = user.id
+        render json: { message: "Sign in is successful"}, status: :created
+      else
+        render json: { errors: "cannot login" }, status: :unprocessable_entity
+      end
+    end
+    
+    def destroy
+      # Log out the user by clearing the session
+      session[:user_id] = nil
+      redirect_to root_path, notice: "Logged out!"
+    end
+end
